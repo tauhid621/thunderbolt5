50499ec9735443e49a96f716911e84a85c2d69be
tauhid621 <tauhidanjum@gmail.com>
2018-02-05 16:48:39 +0530

    Adding pages

---
+++ static/javascript/main.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	$("#logo").on('click', function(event) {
+		var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+		hideBlocksclk(timelineBlocks, offset);
+
+	});
+
+	$(window).on('scroll', function(){
+
+		($(window).scrollTop()<100)&&hideBlocksclk(timelineBlocks, offset);
+
+	});
+
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame)
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function hideBlocksclk(blocks, offset) {
+		blocks.each(function(){
+			$('.cd-timeline-img, .cd-timeline-content').removeClass('bounce-in').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
---
+++ static/javascript/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$('.bolt').css("border-radius","125px");
+});
